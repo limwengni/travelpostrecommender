5d95fdcb9a57956ef47ade81f65a1a2038acefe7
limwengni <115699700+limwengni@users.noreply.github.com>
2024-04-30 19:24:43 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -100,7 +100,7 @@ if st.button("Recommend"):
                     except Exception as e:
                         st.write(f"Error loading image from URL: {full_image_url}")
                         st.write(e)
-            row_html += "</div>"  # Move this line to ensure it's added after each row
+            row_html += "</div>"
             st.write(row_html, unsafe_allow_html=True)
     else:
         st.write("No recommendations found based on your input.")

61ebd08910a586112536bae5b337be6be2174b44
limwengni <115699700+limwengni@users.noreply.github.com>
2024-04-30 18:16:22 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -66,7 +66,6 @@ if st.button("Recommend"):
 
                 # Create HTML to display image with details in a pop-up on click
                 # Create HTML to display image with details in a pop-up on click
-                # Create HTML to display image with details in a pop-up on click
                 html_code = f"""
 <div onclick="showDetails('{recommendation['image_title']}', '{recommendation['location']}', '{recommendation['hashtag']}', '{img_base64_encoded}')" style="cursor: pointer;">
     <img src="data:image/png;base64,{img_base64_encoded}" style="width:250px; height:250px; margin-right:10px; margin-bottom: 10px">
@@ -100,6 +99,7 @@ if st.button("Recommend"):
     }}
 </script>
 """
+
 
                 st.write(html_code, unsafe_allow_html=True)
             except Exception as e:

ef740d52dc7219c34e9b4624c1d9d1c241109d49
limwengni <115699700+limwengni@users.noreply.github.com>
2024-04-30 14:58:57 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -49,14 +49,27 @@ def get_recommendations(location, hashtags_str):
 # Call the recommendation function
 recommendations = get_recommendations(location, hashtags_str)
 
-# Print recommendations if any
-if recommendations:
-  st.subheader("Recommendations:")
-  for recommendation in recommendations:
-    st.write(f"- {recommendation['location']}: {recommendation['hashtag']}")
-    if 'image_url' in recommendation:  # Check if image_url exists
-      st.image(recommendation['image_url'], width=250)  # Display image with width adjustment
-  else:
-    st.write("No recommendations found based on your input.")
-
+base_github_url = "https://github.com/limwengni/travelpostrecommender/blob/main"
+if st.button("Recommend"):
+    # Print recommendations if any
+    recommendations = get_recommendations(location, hashtags_str)
+    if recommendations:
+        st.subheader("Recommendations:")
+        for recommendation in recommendations:
+            st.write(f"- {recommendation['location']}: {recommendation['hashtag']}")
+            # Display the image from GitHub repository using the provided URL
+            image_url = recommendation['image_url']
+            # Modify the URL to the correct format
+            full_image_url = f"{base_github_url}/{image_url}"
+            # Change the URL to view raw content
+            full_image_url = full_image_url.replace("/blob/", "/raw/")
+            try:
+                response = requests.get(full_image_url)
+                img = Image.open(BytesIO(response.content))
+                st.image(img, width=250)
+            except Exception as e:
+                st.write(f"Error loading image from URL: {full_image_url}")
+                st.write(e)
+    else:
+        st.write("No recommendations found based on your input.")
 st.stop()

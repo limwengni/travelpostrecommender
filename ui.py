85490e5fccc3585fb6d6b44df598c89b045ea1c9
limwengni <115699700+limwengni@users.noreply.github.com>
2024-04-30 16:06:34 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -53,16 +53,21 @@ if st.button("Recommend"):
                 index = i * 3 + j
                 if index < num_recommendations:
                     recommendation = recommendations[index]
-                    image_url = recommendation['image_url']
-                    try:
-                        response = requests.get(image_url)
-                        img = Image.open(BytesIO(response.content))
-                        with col1, col2, col3:
-                            st.write(f"- {recommendation['location']}: {recommendation['hashtag']}")
+                    with col1, col2, col3:
+                        st.write(f"- {recommendation['location']}: {recommendation['hashtag']}")
+                        # Display the image from GitHub repository using the provided URL
+                        image_url = recommendation['image_url']
+                        # Modify the URL to the correct format
+                        full_image_url = f"{base_github_url}/{image_url}"
+                        # Change the URL to view raw content
+                        full_image_url = full_image_url.replace("/blob/", "/raw/")
+                        try:
+                            response = requests.get(full_image_url)
+                            img = Image.open(BytesIO(response.content))
                             st.image(img, width=250)
-                    except Exception as e:
-                        st.write(f"Error loading image from URL: {image_url}")
-                        st.write(e)
+                        except Exception as e:
+                            st.write(f"Error loading image from URL: {full_image_url}")
+                            st.write(e)
     else:
-      st.write("No recommendations found based on your input.")
+        st.write("No recommendations found based on your input.")
 st.stop()

8d03055228f2c54e68e23fd29afdc777970d98f0
limwengni <115699700+limwengni@users.noreply.github.com>
2024-04-30 19:22:18 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -13,7 +13,7 @@ st.title("Travel Recommendation App")
 location = st.text_input("Enter Location:")
 hashtags_str = st.text_input("Enter Hashtags (e.g., cultural tours):")
 
-# Define function to get recommendations
+# Call the recommendation function
 def get_recommendations(location, hashtags_str):
     travel = pd.read_csv("image_dataset.csv")
 
@@ -35,11 +35,12 @@ def get_recommendations(location, hashtags_str):
             # Sort entries based on hashtag similarity score
             sorted_df = filtered_df.sort_values(by='hashtag_sim_score', ascending=False)
             # Get top 10 recommendations
-            recommendations = sorted_df[['location', 'hashtag', 'image_url', 'image_title']].head(10).to_dict('records')
+            recommendations = sorted_df[['location', 'hashtag', 'image_url']].head(10).to_dict('records')
             return recommendations
     return []
 
-# Function to convert image to base64
+base_github_url = "https://github.com/limwengni/travelpostrecommender/blob/main"
+
 def image_to_base64(image):
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
@@ -47,8 +48,6 @@ def image_to_base64(image):
     encoded_img = base64.b64encode(buffered.getvalue())
     # Convert the encoded bytes to a string
     return encoded_img.decode('utf-8')
-
-base_github_url = "https://github.com/limwengni/travelpostrecommender/blob/main"
 
 # Call the recommendation function
 if st.button("Recommend"):
@@ -86,16 +85,15 @@ if st.button("Recommend"):
                         # Convert the image to base64
                         img_base64 = image_to_base64(img)
                         # Access recommendation details
-                        img_title = recommendation['image_title']
                         img_hashtag = recommendation['hashtag']
-                        img_location = recommendation['location']
                         # Construct HTML for displaying image with details
                         img_html = f"""
                           <div style="text-align:center;">
-                              <p style="font-weight:bold;">{img_title}</p>
                               <img src="data:image/jpeg;base64,{img_base64}" style="width:250px; height:250px; margin-bottom: 5px;">
-                              <p>Location: {img_location}</p>
-                              <p>Hashtag: #{img_hashtag}</p>
+                              <p style="font-weight:bold;">Hashtag:</p>
+                              <div style="border: 1px solid black; padding: 5px; display: inline-block;">
+                                <p style="margin: 0;">#{img_hashtag}</p>
+                              </div>
                           </div>
                           """
                         row_html += img_html

d80ab943a30504886139fb9cf49f6a0a2f25af69
limwengni <115699700+limwengni@users.noreply.github.com>
2024-05-02 18:19:32 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -102,12 +102,14 @@ if st.button("Recommend"):
                         response = requests.get(full_image_url)
                         if response.status_code == 200:
                             # Display the image
-                            st.image(full_image_url, caption=f"Location: {recommendation['location']}\nHashtag: #{recommendation['hashtag']}\nSimilarity Score: {recommendation['score']}", use_column_width=True)
+                            img = Image.open(BytesIO(response.content))
+                            # Resize the image to 250x250
+                            img = img.resize((250, 250))
+                            st.image(img, caption=f"Location: {recommendation['location']}\nHashtag: #{recommendation['hashtag']}\nSimilarity Score: {recommendation['score']}", use_column_width=False)
                     except Exception as e:
                         st.write(f"Error loading image from URL: {full_image_url}")
                         st.write(e)
 
-            row_html += "</div>"
-            st.html(row_html)
+            st.write("</div>")
     else:
         st.write("No recommendations found based on your input.")
